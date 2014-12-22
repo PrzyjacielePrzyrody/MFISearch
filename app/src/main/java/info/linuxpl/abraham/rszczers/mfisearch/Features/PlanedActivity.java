2e3370b1fb05f258ffd7c0d72bdf2c6b669b5144
Rafał Szczerski <rafal.szczerski@gmail.com>
2014-12-22 22:20:34 +0100

    draft

--- app/src/main/java/info/linuxpl/abraham/rszczers/mfisearch/Features/PlanedActivity.java
+++ app/src/main/java/info/linuxpl/abraham/rszczers/mfisearch/Features/PlanedActivity.java
@@ -10,13 +10,15 @@ import java.util.Date;
  *         <rafal.szczerski(at)gmail.com>
  */
 public abstract class PlanedActivity {
-    protected Date startTime;
+    protected Date date;
+    protected String startTime;
     protected int length;
     protected String instructor;
 
     // Trzeba przeciążyć konstruktor tak, żeby parsował datę ze stringa
     // żeby było wygodniej
-    public PlanedActivity(Date startTime, int length, String instructor) {
+    public PlanedActivity(Date date, String startTime, int length, String instructor) {
+        this.date = date;
         this.startTime = startTime;
         this.length = length;
         this.instructor = instructor;
