1c0511e98429b756917d917a3f3b89a2f4257edf
Rafał Szczerski <rafal.szczerski@gmail.com>
2014-12-25 16:32:17 +0100

    databasehelper methods

--- app/src/main/java/info/linuxpl/abraham/rszczers/mfisearch/Features/SQL/DatabaseAdapter.java
+++ app/src/main/java/info/linuxpl/abraham/rszczers/mfisearch/Features/SQL/DatabaseAdapter.java
@@ -6,5 +6,21 @@ package info.linuxpl.abraham.rszczers.mfisearch.Features.SQL;
  * @author Rafał Szczerski
  *         <rafal.szczerski(at)gmail.com>
  */
+
+import android.content.Context;
+
+/**
+ * Tutaj znajdą się wszystkie metody potrzebne do pobierania danych na potrzeby wyświetlania
+ * tego w różnych elementach interfejsu użytkownika. Pakuj tutaj wszystko, co Ci przyjdzie do głowy,
+ * a czego zabrakło w DatabaseHelper.
+ */
 public class DatabaseAdapter {
+    private DatabaseHelper mfidb;
+
+    public DatabaseAdapter(Context context) {
+        this.mfidb = new DatabaseHelper(context);
+    }
+
+
+
 }
