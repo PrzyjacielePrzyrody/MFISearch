049e53dd830ab930ac520aa280d6ce60b864cec8
Rafał Szczerski <rafal.szczerski@gmail.com>
2014-12-25 18:24:35 +0100

    minor changes

--- app/src/main/java/info/linuxpl/abraham/rszczers/mfisearch/Features/ActivityFactory.java
+++ app/src/main/java/info/linuxpl/abraham/rszczers/mfisearch/Features/ActivityFactory.java
@@ -17,25 +17,39 @@ public class ActivityFactory {
         dbAdapter = new DatabaseAdapter(context);
     }
 
-    public PlanedActivity make(String type, int id, String date, Classroom room, int duration, String instructor,
+    /** KONSTRUKTORY NIE PRZEKAZUJĄ ARGUMENTÓW – DO UZUPEŁNIENIA
+     * Metoda fabrykująca obiekty implementujące PlanedActivity
+     * @param type determinuje jaki rodzaj obiektu chcemy otrzymać; możliwe to: lecture, exercise, exam, other
+     * @param date
+     * @param room
+     * @param duration
+     * @param instructor
+     * @param description
+     * @return
+     */
+    public PlanedActivity make(String type, String date, Classroom room, int duration, String instructor,
                               String description) {
         PlanedActivity product = null;
         type = type.toUpperCase();
         if(type.equals("LECTURE")) {
             product = new LecturePlaned();
             dbAdapter.add(product);
+            //ustawia pole id w obiekcie po nadaniu go w bazie.
             product.setID(dbAdapter.getID(product));
         } else if(type.equals("EXERCISE")) {
             product = new ExercisePlaned();
             dbAdapter.add(product);
+            //ustawia pole id w obiekcie po nadaniu go w bazie.
             product.setID(dbAdapter.getID(product));
         } else if(type.equals("EXAM")) {
             product = new ExamPlaned();
             dbAdapter.add(product);
+            //ustawia pole id w obiekcie po nadaniu go w bazie.
             product.setID(dbAdapter.getID(product));
         } else if(type.equals("OTHER")) {
             product = new OtherPlaned();
             dbAdapter.add(product);
+            //ustawia pole id w obiekcie po nadaniu go w bazie.
             product.setID(dbAdapter.getID(product));
         }
         return product;
